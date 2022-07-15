359854fea3499b88b13abe0a4332fd7bbb466b9d
Matt <matthewbosticdev@gmail.com>
2022-07-15 14:56:24 -0600

    Adding files

--- spring/project2/src/main/java/com/skillstorm/project2/Project2Application.java
+++ spring/project2/src/main/java/com/skillstorm/project2/Project2Application.java
@@ -1,7 +1,9 @@
 package com.skillstorm.project2;
 
+import com.skillstorm.project2.device.DeviceRepository;
 import com.skillstorm.project2.user.UserRepository;
 import com.skillstorm.project2.user.UserService;
+import com.skillstorm.project2.userPlan.UserPlanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,7 +13,12 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Project2Application {
 	@Autowired
-	UserRepository repo;
+	UserRepository userRepo;
+	@Autowired
+	DeviceRepository deviceRepo;
+	@Autowired
+	UserPlanRepository userPlanRepo;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
@@ -20,6 +27,6 @@ public class Project2Application {
 	@Bean
 	CommandLineRunner runner (UserRepository repo){
 
-		 return (args ->  repo.findAll().forEach(System.out::println));
+		 return (args ->  userPlanRepo.findAll().forEach(System.out::println));
 	}
 }
