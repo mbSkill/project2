a62866eea7eb67b6bd4681c3fc968c9e1558a011
Cody Liu <cliu@skillstorm.com>
2022-07-15 17:56:22 -0400

    add plan repo, service, entity

--- spring/project2/src/main/java/com/skillstorm/project2/Project2Application.java
+++ spring/project2/src/main/java/com/skillstorm/project2/Project2Application.java
@@ -1,14 +1,18 @@
 package com.skillstorm.project2;
 
 import com.skillstorm.project2.device.DeviceRepository;
+import com.skillstorm.project2.plan.PlanRepository;
 import com.skillstorm.project2.user.UserRepository;
-import com.skillstorm.project2.user.UserService;
+import com.skillstorm.project2.plan.PlanService;
 import com.skillstorm.project2.userPlan.UserPlanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class Project2Application {
@@ -19,6 +23,8 @@ public class Project2Application {
 	@Autowired
 	UserPlanRepository userPlanRepo;
 
+	@Autowired
+	PlanService planService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
@@ -26,7 +32,12 @@ public class Project2Application {
 
 	@Bean
 	CommandLineRunner runner (UserRepository repo){
+		List<Integer> ids = Arrays.asList(4, 5, 9);
+		System.out.println("-------------------plans begin");
+		System.out.println(planService.findPlanById(ids));
+		System.out.println("-------------------plans end");
 
-		 return (args ->  userPlanRepo.findAll().forEach(System.out::println));
+
+		return (args ->  userPlanRepo.findAll().forEach(System.out::println));
 	}
 }
