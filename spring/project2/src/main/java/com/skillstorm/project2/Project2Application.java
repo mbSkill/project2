141633afd4eac4cb4555727b35317ded74b19f35
Cody Liu <cliu@skillstorm.com>
2022-07-17 23:25:31 -0400

    add controller/userPlanService

--- spring/project2/src/main/java/com/skillstorm/project2/Project2Application.java
+++ spring/project2/src/main/java/com/skillstorm/project2/Project2Application.java
@@ -30,8 +30,9 @@ public class Project2Application {
 		SpringApplication.run(Project2Application.class, args);
 	}
 
-	/*
-	@Bean
+
+
+	/*@Bean
 	CommandLineRunner runner (UserRepository repo){
 		List<Integer> ids = Arrays.asList(4, 5, 9);
 		System.out.println("-------------------plans begin");
@@ -41,5 +42,5 @@ public class Project2Application {
 
 		return (args ->  userPlanRepo.findAll().forEach(System.out::println));
 	}
-	*/
+	 */
 }
---
+++ spring/project2/src/main/java/com/skillstorm/project2/controllers/Project2Controller.java
@@ -0,0 +1,42 @@
+package com.skillstorm.project2.controllers;
+import com.skillstorm.project2.device.DeviceService;
+import com.skillstorm.project2.plan.PlanService;
+import com.skillstorm.project2.user.UserService;
+import com.skillstorm.project2.userPlan.UserPlan;
+import com.skillstorm.project2.userPlan.UserPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Project2Controller {
+    @Autowired
+    private DeviceService deviceService;
+
+    @Autowired
+    private PlanService planService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserPlanService userPlanService;
+
+    public Project2Controller() {
+        System.out.println("controller created!");
+    }
+
+    // Users should be able to sign into their account, and only be able to manage that account
+    //User A should not be able to view/ edit user B’s account
+
+    @GetMapping("/userplans/id")
+    // Users should be able to see a breakdown of their monthly bill based on their plans
+    public ResponseEntity<List<UserPlan>> getUserPlans(int id) {
+        System.out.println("GET user plans");
+        return new ResponseEntity<List<UserPlan>>(HttpStatus.OK);
+    }
+}
