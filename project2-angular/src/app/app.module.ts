585ac61abcb152ddcd42515fa55abd0f5fb855db
Cody Liu <cliu@skillstorm.com>
2022-07-19 17:22:28 -0400

    start angular

---
+++ project2-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserHomepageComponent } from './user-homepage/user-homepage.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserHomepageComponent,
+    UserLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
