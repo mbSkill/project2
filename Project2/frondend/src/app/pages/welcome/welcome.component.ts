8ef3ae0da12db72a4e3cdeeeed46449bc726466b
SS-YiChen <107573468+SS-YiChen@users.noreply.github.com>
2022-07-28 13:33:32 -0400

    welcome page

--- Project2/frondend/src/app/pages/welcome/welcome.component.ts
+++ Project2/frondend/src/app/pages/welcome/welcome.component.ts
@@ -9,9 +9,56 @@ import * as echarts from 'echarts';
 })
 export class WelcomeComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+    type EChartsOption = echarts.EChartsOption;
+
+    var chartDom = document.getElementById('main')!;
+    var myChart = echarts.init(chartDom);
+    var option: EChartsOption;
+
+    option = {
+      title:{
+        text: "2022 - Monthly Payment"
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Jan', 'Feb', 'Mar','Apr', 'May', 'June', 'July', 'Aug','Sep', 'Oct', 'Nov', 'Dec']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [
+            95,
+            {
+              value: 200,
+              itemStyle: {
+                color: '#a90000'
+              }
+            },
+            150,
+            80,
+            70,
+            110,
+            130,
+            120,
+            0,
+            0,
+            0,
+            0,
+            0
+          ],
+          type: 'bar'
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+
     
   }
 
